771b003e9c1b770ba26e853c21c8700e8747fe7f
Jannatul Ferdous Happy <86628137+JannatulHappy@users.noreply.github.com>
2023-11-27 14:05:59 +0600

    up

--- src/pages/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy/PrivacyPolicy.jsx
@@ -42,7 +42,7 @@ const PrivacyPolicy = () => {
       <section className="mb-8">
         <h2 className="mb-2 text-xl font-bold">Information Sharing</h2>
         <p className="text-gray-800">
-          [Your Company Name] respects your privacy and does not sell, trade, or
+          Pollinate respects your privacy and does not sell, trade, or
           share your personal information with third parties without your
           consent, except as required by law.
         </p>
@@ -62,7 +62,7 @@ const PrivacyPolicy = () => {
       <section className="mb-8">
         <h2 className="mb-2 text-xl font-bold">Data Security</h2>
         <p className="text-gray-800">
-          [Your Company Name] employs industry-standard security measures to
+          Pollinate employs industry-standard security measures to
           protect your data from unauthorized access, disclosure, alteration, or
           destruction.
         </p>
@@ -86,52 +86,127 @@ const PrivacyPolicy = () => {
         </p>
       </section>
       <div className="container p-8 mx-auto mt-8 bg-gray-100">
-        <h1 className="mb-4 text-3xl font-bold">Terms of Service</h1>
+        <h1 className="mb-4 text-3xl font-bold text-green-600">
+          Terms of Service
+        </h1>
 
         <section className="mb-8">
-          <h2 className="mb-2 text-xl font-bold">User Responsibilities</h2>
-          <p>
+          <h2 className="mb-2 text-xl font-bold">1. User Responsibilities</h2>
+          <p className="text-gray-700">
             By using Pollinate's services, you agree to the following
             responsibilities:
           </p>
           <ul className="ml-6 list-disc">
-            <li>Respect the rights and privacy of other users.</li>
-            <li>Adhere to community guidelines and standards.</li>
-            <li>
+            <li className="text-gray-700">
+              Respect the rights and privacy of other users.
+            </li>
+            <li className="text-gray-700">
+              Adhere to community guidelines and standards.
+            </li>
+            <li className="text-gray-700">
               Provide accurate and truthful information when using our platform.
             </li>
           </ul>
         </section>
 
         <section className="mb-8">
-          <h2 className="mb-2 text-xl font-bold">Pro-User Memberships</h2>
-          <p>
+          <h2 className="mb-2 text-xl font-bold">2. Pro-User Memberships</h2>
+          <p className="text-gray-700">
             Users who opt for a Pro membership enjoy additional benefits,
             including:
           </p>
           <ul className="ml-6 list-disc">
-            <li>
+            <li className="text-gray-700">
               Access to premium features and advanced survey creation tools.
             </li>
-            <li>Enhanced user privileges for a more tailored experience.</li>
-            <li>Priority customer support for timely assistance.</li>
+            <li className="text-gray-700">
+              Enhanced user privileges for a more tailored experience.
+            </li>
+            <li className="text-gray-700">
+              Priority customer support for timely assistance.
+            </li>
           </ul>
         </section>
-
         <section className="mb-8">
-          <h2 className="mb-2 text-xl font-bold">Prohibited Activities</h2>
-          <p>
+          <h2 className="mb-2 text-xl font-bold">
+            11. Surveyor Responsibilities
+          </h2>
+          <p className="text-gray-700">
+            Surveyors on Pollinate have the responsibility to create and manage
+            surveys ethically and within the guidelines set by the platform.
+            This includes ensuring the content is appropriate, unbiased, and
+            complies with our community standards.
+          </p>
+        </section>
+        <section className="mb-8">
+          <h2 className="mb-2 text-xl font-bold">3. Prohibited Activities</h2>
+          <p className="text-gray-700">
             Users are prohibited from engaging in activities that violate our
             terms, including but not limited to:
           </p>
           <ul className="ml-6 list-disc">
-            <li>
+            <li className="text-gray-700">
               Attempting to manipulate survey results or engage in fraudulent
               activities.
             </li>
-            <li>Disrupting the normal functioning of the platform.</li>
-            <li>Violating the rights of other users or third parties.</li>
+            <li className="text-gray-700">
+              Disrupting the normal functioning of the platform.
+            </li>
+            <li className="text-gray-700">
+              Violating the rights of other users or third parties.
+            </li>
           </ul>
+        </section>
+
+        <section className="mb-8">
+          <h2 className="mb-2 text-xl font-bold">4. Content Ownership</h2>
+          <p className="text-gray-700">
+            Users retain ownership of the content they create on Pollinate's
+            platform. However, by uploading content, you grant Pollinate a
+            worldwide license to use, distribute, and display that content.
+          </p>
+        </section>
+
+        <section className="mb-8">
+          <h2 className="mb-2 text-xl font-bold">5. Termination of Accounts</h2>
+          <p className="text-gray-700">
+            Pollinate reserves the right to terminate user accounts
+            that violate our terms of service or community guidelines.
+          </p>
+        </section>
+
+        <section className="mb-8">
+          <h2 className="mb-2 text-xl font-bold">6. Modifications to Terms</h2>
+          <p className="text-gray-700">
+            Pollinate may revise these terms of service at any time.
+            Users will be notified of significant changes, and continued use of
+            the platform constitutes acceptance of the modified terms.
+          </p>
+        </section>
+        <section className="mb-8">
+          <h2 className="mb-2 text-xl font-bold">8. Disclaimer of Warranty</h2>
+          <p className="text-gray-700">
+            Pollinate provides its services on an "as-is" and
+            "as-available" basis. We do not warrant that the platform will be
+            error-free or uninterrupted.
+          </p>
+        </section>
+
+        <section className="mb-8">
+          <h2 className="mb-2 text-xl font-bold">9. Limitation of Liability</h2>
+          <p className="text-gray-700">
+            Pollinate shall not be liable for any indirect,
+            incidental, special, consequential, or punitive damages, or any loss
+            of profits or revenues.
+          </p>
+        </section>
+
+        <section className="mb-8">
+          <h2 className="mb-2 text-xl font-bold">10. Governing Law</h2>
+          <p className="text-gray-700">
+            These terms of service are governed by and construed in accordance
+            with the laws of [Your Country/State].
+          </p>
         </section>
 
         {/* Add more sections as needed */}
