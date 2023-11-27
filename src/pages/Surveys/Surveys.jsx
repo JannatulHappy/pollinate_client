67fdb2338c167fe008b894b3982e29aefacd78ee
Jannatul Ferdous Happy <86628137+JannatulHappy@users.noreply.github.com>
2023-11-27 18:03:08 +0600

    fixed survey page

--- src/pages/Surveys/Surveys.jsx
+++ src/pages/Surveys/Surveys.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import useSurveys from "../../hooks/useSurveys";
+import SurveyCard from "../../components/Shared/SurveyCard";
+import Container from "../../components/Shared/Container";
 
 const Surveys = () => {
   const { data: surveys, isLoading } = useSurveys();
@@ -13,9 +15,12 @@ const Surveys = () => {
     // Check if surveys data is available
     if (!surveys) return;
 
-    // Set filtered surveys based on selected category, title, and vote
-    let filtered = [...surveys];
+    // Filter surveys based on published status
+    let filtered = surveys.filter(
+      (survey) => survey.surveyStatus === "published"
+    );
 
+    // Set filtered surveys based on selected category, title, and vote
     if (filterCategory) {
       filtered = filtered.filter(
         (survey) =>
@@ -48,72 +53,71 @@ const Surveys = () => {
 
   return (
     <div>
-      {/* Filtering options */}
-      <div className="flex mb-4 space-x-4">
-        {/* Category Dropdown */}
-        <select
-          value={filterCategory}
-          onChange={(e) => setFilterCategory(e.target.value)}
-          className="p-2 border"
-        >
-          <option value="">All Categories</option>
-          {[...new Set(surveys.map((survey) => survey.category))].map(
-            (category) => (
-              <option key={category} value={category}>
-                {category}
-              </option>
-            )
-          )}
-        </select>
+      <Container>
+        {/* Filtering options */}
+        <div className="flex mb-4 space-x-4">
+          {/* Category Dropdown */}
+          <select
+            value={filterCategory}
+            onChange={(e) => setFilterCategory(e.target.value)}
+            className="p-2 border"
+          >
+            <option value="">All Categories</option>
+            {[...new Set(surveys.map((survey) => survey.category))].map(
+              (category) => (
+                <option key={category} value={category}>
+                  {category}
+                </option>
+              )
+            )}
+          </select>
 
-        {/* Title Dropdown */}
-        <select
-          value={filterTitle}
-          onChange={(e) => setFilterTitle(e.target.value)}
-          className="p-2 border"
-        >
-          <option value="">Filter By Titles</option>
-          {[...new Set(surveys.map((survey) => survey.title))].map((title) => (
-            <option key={title} value={title}>
-              {title}
-            </option>
-          ))}
-        </select>
+          {/* Title Dropdown */}
+          <select
+            value={filterTitle}
+            onChange={(e) => setFilterTitle(e.target.value)}
+            className="p-2 border"
+          >
+            <option value="">Filter By Titles</option>
+            {[...new Set(surveys.map((survey) => survey.title))].map(
+              (title) => (
+                <option key={title} value={title}>
+                  {title}
+                </option>
+              )
+            )}
+          </select>
 
-        {/* Vote Dropdown */}
-        <select
-          value={filterVote}
-          onChange={(e) => setFilterVote(e.target.value)}
-          className="p-2 border"
-        >
-          <option value="">All Votes</option>
-          {[...new Set(surveys.map((survey) => survey.totalVote))].map((vote) => (
-            <option key={vote} value={vote}>
-              {vote}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      {/* Display surveys or show message if no matching surveys */}
-      {noMatchingSurveys ? (
-        <p>No matching surveys found.</p>
-      ) : (
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-          {filteredSurveys.map((survey) => (
-            <div key={survey.title} className="p-4 mb-4 border">
-              <img src={survey.image} alt={survey.title} className="mb-4" />
-              <h2 className="text-xl font-bold">{survey.title}</h2>
-              <p>{survey.description}</p>
-              <p>Total Voted: {survey.totalVote}</p>
-              <p>Category: {survey.category}</p>
-              <p>Like: {survey.totalVote}</p>
-              <p>Dislike: {survey.dislike}</p>
-              {/* Add more survey details as needed */}
-            </div>
-          ))}
+          {/* Vote Dropdown */}
+          <select
+            value={filterVote}
+            onChange={(e) => setFilterVote(e.target.value)}
+            className="p-2 border"
+          >
+            <option value="">All Votes</option>
+            {[...new Set(surveys.map((survey) => survey.totalVote))].map(
+              (vote) => (
+                <option key={vote} value={vote}>
+                  {vote}
+                </option>
+              )
+            )}
+          </select>
         </div>
-      )}
+        {/* Display surveys or show message if no matching surveys */}
+        {noMatchingSurveys ? (
+          <p>No matching surveys found.</p>
+        ) : (
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+            {filteredSurveys.map((survey, index) => (
+              <div key={index}>
+                {" "}
+                <SurveyCard survey={survey}></SurveyCard>
+              </div>
+            ))}
+          </div>
+        )}
+      </Container>
     </div>
   );
 };
