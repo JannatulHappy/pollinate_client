9d5f1efa36a8369ea05877b81550c6609df3eace
Jannatul Ferdous Happy <86628137+JannatulHappy@users.noreply.github.com>
2023-11-28 18:20:09 +0600

    up

--- src/pages/Surveys/SurveyDetails/SurveyDetails.jsx
+++ src/pages/Surveys/SurveyDetails/SurveyDetails.jsx
@@ -7,6 +7,7 @@ import useRole from "../../../hooks/useRole";
 import Container from "../../../components/Shared/Container";
 import Loading from "../../../components/Shared/Loading";
 import ReactApexChart from "react-apexcharts";
+import moment from "moment";
 
 const SurveyDetails = () => {
   const { user } = useAuth();
@@ -17,7 +18,7 @@ const SurveyDetails = () => {
   const [selectedOptions, setSelectedOptions] = useState({});
   const [comment, setComment] = useState(null);
   const [isLike, setIsLike] = useState("");
-
+  // const [report, setReport] = useState(false);
   const [isInclude, setIsInclude] = useState(false);
 
   const { isLoading, isError, data, error } = useQuery({
@@ -29,12 +30,21 @@ const SurveyDetails = () => {
     const response = data?.responses.map(
       (response) => response.responseUserEmail
     );
-    console.log("response email paici", response);
+    // console.log("response email paici", response);
     if (response?.includes(loggedUserData?.email)) {
       setIsInclude(true);
     }
   }, [data, loggedUserData]);
+  const isDatePassed = (timestamp) => {
+    const today = moment();
+    const timestampDate = moment(timestamp);
 
+    // Compare today's date with the timestamp date
+    return today.isAfter(timestampDate);
+  };
+  const timePassed = isDatePassed(data?.timestamp);
+
+  // Output: false (if today is before October 1, 2023)
   const handleVote = async (e) => {
     e.preventDefault();
 
@@ -43,36 +53,43 @@ const SurveyDetails = () => {
     }
 
     if (user) {
-      // Implement logic to submit the vote based on selectedOptions
-      if (comment) {
-        console.log("Feedback: ", [
-          {
-            name: user?.displayName || loggedUserData?.name,
-            proUserEmail: user?.email,
-            proUserImage: user?.photoURL,
-            comment: comment,
-          },
-        ]);
-      }
      
-     const like= isLike==="yes"?data?.like+1 :data?.like
-     const dislike= isLike==="no"?data?.dislike+1:data.dislike
-     console.log("answers :", selectedOptions);
-      console.log("TotalVote :", data.totalVote + 1);
-      console.log("like:",like, "dislike:", dislike)
-    
+      
 
-      // Assuming you have an async function to submit the vote
-      try {
-        // Replace the following line with actual logic to submit the vote
-        // await submitVote(selectedOptions, comment);
-        setSubmitVote("yes");
-      } catch (error) {
-        console.error("Error submitting vote:", error);
-        // Handle error if vote submission fails
-        // You might want to reset submitVote to "no" here
-      }
+
+      // const dislike = isLike === "no" ? data?.dislike + 1 : data.dislike;
+
+      // console.log("answers :", selectedOptions);
+      // console.log("TotalVote :", data.totalVote + 1);
+      // console.log("like:", like, "dislike:", dislike);
+      const updatedData = {
+        like: isLike === "yes" ? data?.like + 1 : data?.like,
+        dislike: isLike === "no" ? data?.dislike + 1 : data.dislike,
+
+        totalVote: data.totalVote + 1,
+
+        Feedback: {
+          proUserName: user?.displayName || loggedUserData?.name,
+          proUserEmail: user?.email,
+          proUserImage: user?.photoURL,
+          comment: comment,
+        },
+
+        responses: [
+          {
+            responseUserEmail: user.email,
+            answers: {
+              question1: selectedOptions.question1,
+              question2: selectedOptions.question2,
+              question3: selectedOptions.question3,
+            },
+          },
+        ],
+      };
+      setSubmitVote("yes");
+      console.log("updated", updatedData);
     }
+
     //   todo:call refetch after submit the data to backend
   };
 
@@ -84,6 +101,8 @@ const SurveyDetails = () => {
     return <span className="text-white">Error: {error.message}</span>;
   }
 
+  // console.log("time pass hoice?",isTimePassed)
+  // console.log("time pass hoice?", timePassed);
   const survey = data;
 
   // Define chart options
@@ -116,8 +135,8 @@ const SurveyDetails = () => {
     },
   ];
 
-  console.log("paici", submitVote);
-  console.log("isinclude", isInclude);
+  // console.log("paici", submitVote);
+  // console.log("isinclude", isInclude);
   return (
     <Container>
       <div className="flex">
@@ -151,8 +170,13 @@ const SurveyDetails = () => {
         </div>
 
         {/* Render chart if isInclude is true */}
-        {isInclude ? (
+        {isInclude || timePassed ? (
           <div className="w-1/2">
+            {timePassed && (
+              <p className="py-10 text-base font-medium text-red-500">
+                The Deadline is expires of this Survey!!!
+              </p>
+            )}
             <h2 className="mb-4 text-xl font-bold">Survey Results Chart:</h2>
             <ReactApexChart
               options={chartOptions}
@@ -195,7 +219,9 @@ const SurveyDetails = () => {
                 ))}
               </div>
               <div className="space-x-4 ">
-                <p className="text-lg font-bold">Do You like this Survey?</p>
+                <p className="text-lg font-bold text-green-600">
+                  Do You like this Survey?
+                </p>
                 <select
                   className="px-4 py-2 bg-gray-300 rounded-md"
                   value={isLike}
@@ -233,7 +259,7 @@ const SurveyDetails = () => {
                   </button>
                 </div>
               ) : (
-                <div className="mb-4">
+                <div className="my-6">
                   <button
                     type="submit"
                     disabled
